e37345f0f7528774e000365d18a4a9892cd8fb7b
Vikram Kangotra <vikramkangotra8055@gmail.com>
2024-02-09 20:16:55 +0530

    Added 3D

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,68 @@
 mod solar_system;
 
-use bevy::prelude::*;
+use bevy::{input::{keyboard, mouse::MouseMotion}, prelude::*, window::{CursorGrabMode, PrimaryWindow}};
 use solar_system::SolarSystemPlugin;
+
+#[derive(Component)]
+struct MyCamera;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_plugins((DefaultPlugins, SolarSystemPlugin))
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (setup, cursor_grab))
+        .add_systems(Update, navigation)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Camera3dBundle {
+            projection: PerspectiveProjection {
+                fov: 60.0_f32.to_radians(),
+                ..Default::default()
+            }.into(),
+            transform: Transform::from_translation(Vec3::new(10.0, 30.0, 50.0))
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        },
+        MyCamera,
+    ));
+}
+
+fn cursor_grab(mut windows: Query<&mut Window, With<PrimaryWindow>>) {
+
+    let mut primary_window = windows.single_mut();
+    primary_window.cursor.grab_mode = CursorGrabMode::Locked;
+    primary_window.cursor.visible = false;
+}
+
+fn navigation(
+    mut keyboard_evr: EventReader<keyboard::KeyboardInput>,
+    mut motion_evr: EventReader<MouseMotion>,
+    mut camera: Query<&mut Transform, With<MyCamera>>,
 ) {
 
-    let background_image: Handle<Image> = asset_server.load("textures/stars_milky_way.jpg");
+    let mut camera_transform = camera.single_mut();
 
-    commands.spawn(Camera2dBundle::default())
-        .insert(SpriteBundle {
-            texture: background_image,
-            transform: Transform::from_translation(Vec3::new(0., 0., -1.)),
-            ..Default::default()
-        });
+    for motion in motion_evr.read() {
+        camera_transform.rotate(Quat::from_rotation_x(-motion.delta.y * 5e-4));
+        camera_transform.rotate(Quat::from_rotation_y(-motion.delta.x * 5e-4));
+    }
+
+    for event in keyboard_evr.read() {
+        let mut translation = Vec3::ZERO;
+        if event.state.is_pressed() {
+            match event.key_code {
+                Some(keyboard::KeyCode::W) => translation.z -= 1.0,
+                Some(keyboard::KeyCode::A) => translation.x -= 1.0,
+                Some(keyboard::KeyCode::S) => translation.z += 1.0,
+                Some(keyboard::KeyCode::D) => translation.x += 1.0,
+                Some(keyboard::KeyCode::Space) => translation.y += 1.0,
+                _ => {}
+            }
+        }
+        let translation = camera_transform.rotation * translation;
+        camera_transform.translation += translation;
+    }
 }
--- src/solar_system.rs
+++ src/solar_system.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::prelude::*;
 
 #[derive(Component)]
 struct Planet;
@@ -19,66 +19,149 @@ pub struct SolarSystemPlugin;
 
 impl Plugin for SolarSystemPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_planet)
+        app.add_systems(Startup, (add_sun, add_planet, add_star))
             .add_systems(Update, update_position);
     }
+}
+
+fn add_star(
+    mut commands: Commands,
+    server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let star_handle: Handle<Image> = server.load("textures/stars_milky_way.jpg");
+
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::from(shape::UVSphere {
+            radius: 1000.0,
+            sectors: 50,
+            stacks: 50,
+        })),
+        material: materials.add(StandardMaterial {
+            base_color_texture: Some(star_handle),
+            unlit: true,
+            cull_mode: None,
+            ..Default::default()
+        }),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..Default::default()
+    });
+}
+
+fn add_sun(
+    mut commands: Commands, 
+    server: Res<AssetServer>, 
+    mut meshes: ResMut<Assets<Mesh>>, 
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    let sun_handle: Handle<Image> = server.load("textures/sun.jpg");
+
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(shape::UVSphere {
+                radius: 5.0,
+                sectors: 50,
+                stacks: 50,
+            }.into()),
+            material: materials.add(
+                StandardMaterial {
+                    base_color_texture: Some(sun_handle),
+                    emissive: Color::WHITE,
+                    unlit: true,
+                    ..Default::default()
+                }
+            ),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..Default::default()
+        },
+        Planet,
+        PlanetType::Sun,
+    ));
+
+    commands.spawn(
+        PointLightBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            point_light: PointLight {
+                intensity: 10000.0,
+                range: 1000.0,
+                ..Default::default()
+            },
+            ..Default::default()
+        }
+    );
 }
 
 fn add_planet(
     mut commands: Commands, 
     server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>, 
-    mut materials: ResMut<Assets<ColorMaterial>>
+    mut materials: ResMut<Assets<StandardMaterial>>
 ) {
 
-    let sun_handle: Handle<Image> = server.load("textures/sun.jpg");
     let mercury_handle: Handle<Image> = server.load("textures/mercury.jpg");
     let venus_handle: Handle<Image> = server.load("textures/venus.jpg");
     let earth_handle: Handle<Image> = server.load("textures/earth.jpg");
     let mars_handle: Handle<Image> = server.load("textures/mars.jpg");
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(sun_handle.into()),
-        transform: Transform::from_translation(Vec3::new(0.0, 0.0, 1.0)),
-        ..Default::default()
-    })
-    .insert(Planet)
-    .insert(PlanetType::Sun);
-
     let planets = vec![
-        (10., PlanetType::Mercury, 100.0, Vec3::new(0., 3., 0.), mercury_handle),
-        (14., PlanetType::Venus, 200.0, Vec3::new(0., 2., 0.), venus_handle),
-        (20., PlanetType::Earth, 300.0, Vec3::new(0., 1.5, 0.), earth_handle),
-        (16., PlanetType::Mars, 400.0, Vec3::new(0., 1.5, 0.), mars_handle),
+        (PlanetType::Mercury, mercury_handle, 1.0, Vec3::new(10.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 10.)),
+        (PlanetType::Venus, venus_handle, 2.5, Vec3::new(20.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 7.)),
+        (PlanetType::Earth, earth_handle, 3.0, Vec3::new(30.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 6.)),
+        (PlanetType::Mars, mars_handle, 2.0, Vec3::new(40.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 5.)),
     ];
 
-    for (size, planet_type, distance, velocity, texture) in planets {
+    for (planet_type, texture, size, position, velocity) in planets {
         commands.spawn((
-            Planet, 
-            planet_type, 
-            Velocity(velocity), 
-            MaterialMesh2dBundle {
-                mesh: meshes.add(shape::Circle::new(size).into()).into(),
-                material: materials.add(texture.into()),
-                transform: Transform::from_translation(Vec3::new(distance, 0.0, 0.0)),
+            PbrBundle {
+                mesh: meshes.add(shape::UVSphere {
+                    radius: size,
+                    sectors: 50,
+                    stacks: 50,
+                }.into()),
+                material: materials.add(
+                    StandardMaterial {
+                        base_color_texture: Some(texture),
+                        ..Default::default()
+                    }
+                ),
+                transform: Transform::from_translation(position),
                 ..Default::default()
-            })
-        );
+            },
+            Planet,
+            planet_type,
+            Velocity(velocity),
+        ));
     }
 }
 
-fn update_position(mut query: Query<(&mut Transform, &PlanetType, &mut Velocity), With<Planet>>) {
+fn update_position(
+    mut query: Query<(&mut Transform, &PlanetType, &mut Velocity), With<Planet>>,
+    time: Res<Time>
+) {
 
     let sun_position = Vec3::ZERO;
     let sun_mass = 1e6;
-    let gravitational_constant = 1e-3;
+    let gravitational_constant = 1e-5;
 
-    for (mut transform, _, mut velocity) in query.iter_mut() {
+    for (mut transform, planet_type, mut velocity) in query.iter_mut() {
+        let rotation_speed = match planet_type {
+            PlanetType::Mercury => 0.01,
+            PlanetType::Venus => 0.005,
+            PlanetType::Earth => 0.003,
+            PlanetType::Mars => 0.002,
+            _ => 0.0,
+        };
+
+        transform.rotate(Quat::from_rotation_y(rotation_speed));
+
         let distance = sun_position - transform.translation;
         let direction = distance.normalize();
-        let acceleration = gravitational_constant * sun_mass * direction / distance.length_squared();
+
+        let acceleration = gravitational_constant * sun_mass / distance.length_squared() * direction;
+
         velocity.0 += acceleration;
-        transform.translation += velocity.0;
+
+        transform.translation += velocity.0 * time.delta_seconds();
     }
 }
